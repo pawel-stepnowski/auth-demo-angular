b2ef574ef2af0f710d2e358bc4e03081fc5f068a
pawel-stepnowski <p.m.stepnowski@gmail.com>
2025-02-05 10:46:53 +0100

    General code improvements.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+import { AuthenticationService } from '@liquescens/auth-angular';
+
+function loadConfiguration()
+{
+  
+}
 
 export const appConfig: ApplicationConfig =
 {
   providers:
   [
-    { provide: 'authentication-configuration', useValue: environment.authentication },
+    // { provide: 'authentication-configuration', useValue: environment.authentication },
+    { provide: 'authentication-configuration', useFactory: loadConfiguration },
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes)
   ]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,9 +4,8 @@ export const environment =
 {
     authentication:
     {
-        id: 'b0897f7e-c42c-40e0-afda-0394ad72d668',
-        host: 'https://auth-demo-express-1011928235427.us-central1.run.app',
-        redirect_uri: 'https://auth-demo-express-1011928235427.us-central1.run.app/auth',
+        host: 'https://127.0.0.1:8080',
+        redirect_uri: 'https://127.0.0.1:8080/auth',
         providers:
         [
             new Auth.OAuth2.Google({ client_id: '1011928235427-2qva1pc8n3ndo2karcj8fmcj9co9oqgq.apps.googleusercontent.com' }),
